acf89def55fcb5440f4772f4b041d240263cebba
Jeff McMorris <delay@jeffs-mbp.lan>
2020-05-17 20:39:51 -0500

    reorder packages

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,9 @@
+import 'package:flutter/material.dart';
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_starter/models/models.dart';
 import 'package:simple_gravatar/simple_gravatar.dart';
+import 'package:flutter_starter/models/models.dart';
 
 class AuthService extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
